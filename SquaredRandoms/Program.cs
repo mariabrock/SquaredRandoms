341b23a485f6808c62ad81d12ed9b7410f8bdc6d
Maria <mariabrock414@gmail.com>
2020-03-21 11:06:25 -0500

    Add project files.

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumbers = GenerateAListOfRandomNumbers();
+            var squaredNums =  randomNumbers.Select(number => number * number);  //transforms
+            var evenNumbers = squaredNums.Where(squareNum => squareNum % 2 == 0); //filters
+            Console.ReadKey();
+        }
+
+        private static List<int> GenerateAListOfRandomNumbers()
+        {
+            Random random = new Random();
+            var randNums = new List<int>();
+            for (var i = 0; i < 20; i++)
+            {
+                var number = random.Next(1, 50);
+                randNums.Add(number);
+            }
+            return randNums;
+
+            
+        }
+    }
+}
